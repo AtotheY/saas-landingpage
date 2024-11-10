8b2e4662ed371398e96b5421e6d3fe762adc15ec
Collins <149684399+coll254-strath@users.noreply.github.com>
2024-11-10 17:31:10 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
   twitter: {
     card: 'summary_large_image',
     title:
-      'Ant-Team - Information you need during on-call emergencies',
+      'Ant-Team : Information you need during on-call emergencies',
     description:
       'Quickly link new on-call tickets to similar past incidents and their solutions. All directly in Slack the moment an incident happens.',
     images: ['https://i.imgur.com/MPMcyPP.png']
